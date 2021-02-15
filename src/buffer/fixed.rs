3542d558d1225bced7d1bb35565dfacf3ca1d2dd
Mark LeMoine <linclelinkpart5@gmail.com>
2021-02-15 00:33:03 -0800

    Add `Iter`/`IterMut` to `fixed`, add `iter`/`iter_mut` to `Fixed`, add helper methods to assist

--- src/buffer/fixed.rs
+++ src/buffer/fixed.rs
@@ -1,4 +1,6 @@
+use std::iter::FusedIterator;
 use std::marker::PhantomData;
+use std::slice::{Iter as SliceIter, IterMut as SliceIterMut};
 
 /// A ring buffer (also known as a circular/cyclic buffer) with a fixed capacity
 /// and FIFO semantics.
@@ -63,6 +65,7 @@ where
     ///     let buffer = Fixed::from([1, 2, 3, 4]);
     ///     assert_eq!(buffer.capacity(), 4);
     /// }
+    /// ```
     #[inline]
     pub fn capacity(&self) -> usize {
         self.buffer.as_ref().len()
@@ -222,6 +225,34 @@ where
         let (_, buffer) = self.into_raw_parts();
         buffer
     }
+
+    fn as_slices(&self) -> (&[E], &[E]) {
+        let (tail, head) = self.buffer.as_ref().split_at(self.head);
+        (head, tail)
+    }
+
+    fn as_slices_mut(&mut self) -> (&mut [E], &mut [E]) {
+        let (tail, head) = self.buffer.as_mut().split_at_mut(self.head);
+        (head, tail)
+    }
+
+    pub fn iter(&self) -> Iter<'_, E> {
+        let (head, tail) = self.as_slices();
+
+        Iter {
+            head: head.iter(),
+            tail: tail.iter(),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, E> {
+        let (head, tail) = self.as_slices_mut();
+
+        IterMut {
+            head: head.iter_mut(),
+            tail: tail.iter_mut(),
+        }
+    }
 }
 
 impl<E, B> From<B> for Fixed<E, B>
@@ -263,5 +294,180 @@ where
 {
     fn as_mut(&mut self) -> &mut [E] {
         self.buffer.as_mut()
+    }
+}
+
+pub struct Iter<'a, E>
+where
+    E: Copy + PartialEq,
+{
+    head: SliceIter<'a, E>,
+    tail: SliceIter<'a, E>,
+}
+
+impl<'a, E> Iterator for Iter<'a, E>
+where
+    E: Copy + PartialEq,
+{
+    type Item = &'a E;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.head.next().or_else(|| self.tail.next())
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.len();
+        (len, Some(len))
+    }
+}
+
+impl<'a, E> ExactSizeIterator for Iter<'a, E>
+where
+    E: Copy + PartialEq,
+{
+    fn len(&self) -> usize {
+        self.head.len() + self.tail.len()
+    }
+}
+
+impl<'a, E> DoubleEndedIterator for Iter<'a, E>
+where
+    E: Copy + PartialEq,
+{
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.tail.next_back().or_else(|| self.head.next_back())
+    }
+}
+
+impl<'a, E> FusedIterator for Iter<'a, E>
+where
+    E: Copy + PartialEq,
+{}
+
+pub struct IterMut<'a, E>
+where
+    E: Copy + PartialEq,
+{
+    head: SliceIterMut<'a, E>,
+    tail: SliceIterMut<'a, E>,
+}
+
+impl<'a, E> Iterator for IterMut<'a, E>
+where
+    E: Copy + PartialEq,
+{
+    type Item = &'a mut E;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.head.next().or_else(|| self.tail.next())
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.len();
+        (len, Some(len))
+    }
+}
+
+impl<'a, E> ExactSizeIterator for IterMut<'a, E>
+where
+    E: Copy + PartialEq,
+{
+    fn len(&self) -> usize {
+        self.head.len() + self.tail.len()
+    }
+}
+
+impl<'a, E> DoubleEndedIterator for IterMut<'a, E>
+where
+    E: Copy + PartialEq,
+{
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.tail.next_back().or_else(|| self.head.next_back())
+    }
+}
+
+impl<'a, E> FusedIterator for IterMut<'a, E>
+where
+    E: Copy + PartialEq,
+{}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn iter() {
+        let head = [1, 2, 3];
+        let tail = [4, 5, 6];
+
+        let iter = Iter {
+            head: head.iter(),
+            tail: tail.iter(),
+        };
+
+        assert_eq!(iter.collect::<Vec<_>>(), vec![&1, &2, &3, &4, &5, &6]);
+
+        let iter_rev = Iter {
+            head: head.iter(),
+            tail: tail.iter(),
+        }.rev();
+
+        assert_eq!(iter_rev.collect::<Vec<_>>(), vec![&6, &5, &4, &3, &2, &1]);
+
+        let mut iter = Iter {
+            head: head.iter(),
+            tail: tail.iter(),
+        };
+
+        assert_eq!(iter.len(), 6);
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next_back(), Some(&6));
+        assert_eq!(iter.len(), 4);
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next_back(), Some(&5));
+        assert_eq!(iter.len(), 2);
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next_back(), Some(&4));
+        assert_eq!(iter.len(), 0);
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next_back(), None);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut head = [1, 2, 3];
+        let mut tail = [4, 5, 6];
+
+        let iter = IterMut {
+            head: head.iter_mut(),
+            tail: tail.iter_mut(),
+        };
+
+        assert_eq!(iter.collect::<Vec<_>>(), vec![&mut 1, &mut 2, &mut 3, &mut 4, &mut 5, &mut 6]);
+
+        let iter_rev = IterMut {
+            head: head.iter_mut(),
+            tail: tail.iter_mut(),
+        }.rev();
+
+        assert_eq!(iter_rev.collect::<Vec<_>>(), vec![&mut 6, &mut 5, &mut 4, &mut 3, &mut 2, &mut 1]);
+
+        let mut iter = IterMut {
+            head: head.iter_mut(),
+            tail: tail.iter_mut(),
+        };
+
+        assert_eq!(iter.len(), 6);
+        assert_eq!(iter.next(), Some(&mut 1));
+        assert_eq!(iter.next_back(), Some(&mut 6));
+        assert_eq!(iter.len(), 4);
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next_back(), Some(&mut 5));
+        assert_eq!(iter.len(), 2);
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next_back(), Some(&mut 4));
+        assert_eq!(iter.len(), 0);
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next_back(), None);
     }
 }
