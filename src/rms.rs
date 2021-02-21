b3bc7b403f0ae398894f600a85e18b2d6ac384c2
Mark LeMoine <linclelinkpart5@gmail.com>
2021-02-20 18:13:55 -0800

    Add `Rms::current` add initial doc comments

--- src/rms.rs
+++ src/rms.rs
@@ -3,6 +3,8 @@ use num_traits::Float;
 use crate::{Frame, Sample};
 use crate::buffer::{Fixed, Storage};
 
+/// Keeps a running RMS (root mean square) over a window of [`Frame`]s over
+/// time.
 #[derive(Clone)]
 pub struct Rms<F, S, const N: usize>
 where
@@ -18,6 +20,9 @@ where
     F: Frame<N>,
     S: Storage<Item = F::Float>,
 {
+    /// Creates a new [`Rms`] using a given [`Fixed`] ring buffer as a window.
+    /// The initial contents of the buffer will be overwritten with
+    /// equilibrium values.
     pub fn new(buffer: Fixed<S>) -> Self {
         Self {
             window: buffer,
@@ -47,20 +52,25 @@ where
     pub fn next_squared(&mut self, new_frame: F) -> F::Float {
         // Determine the square of the new frame.
         let new_frame_square = new_frame.into_float_frame().apply(|s| s * s);
+
         // Push back the new frame_square.
         let removed_frame_square = self.window.push(new_frame_square);
+
         // Add the new frame square and subtract the removed frame square.
         self.square_sum =
             self.square_sum
                 .add_frame(new_frame_square.into_signed_frame())
                 .zip_apply(removed_frame_square, |s, r| {
-                    let diff = s - r;
                     // In case of floating point rounding errors, floor at
                     // equilibrium.
-                    diff.max(Sample::EQUILIBRIUM)
+                    (s - r).max(Sample::EQUILIBRIUM)
                 });
 
         self.calc_rms_squared()
+    }
+
+    pub fn current(&self) -> F::Float {
+        self.calc_rms_squared().apply(Float::sqrt)
     }
 
     fn calc_rms_squared(&self) -> F::Float {
