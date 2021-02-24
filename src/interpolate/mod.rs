b49ef5d0cdd2b61231b7b30590fbfa170baf080c
Mark LeMoine <linclelinkpart5@gmail.com>
2021-02-24 12:05:13 -0800

    Add `Interpolator` trait

---
+++ src/interpolate/mod.rs
@@ -0,0 +1,18 @@
+use crate::Frame;
+
+/// Types that can interpolate between two [`Frame`]s.
+///
+/// Implementations should keep track of any necessary data both before and
+/// after the current [`Frame`].
+pub trait Interpolator<const N: usize> {
+    /// The type of frame over which to iterpolate.
+    type Frame: Frame<N>;
+
+    /// Given a value in the interval [0.0, 1.0) representing the fractional
+    /// position between the two interpolated [`Frame`]s, return the
+    /// interpolated [`Frame`].
+    fn interpolate(&self, x: f64) -> Self::Frame;
+
+    /// To be called whenever the interpolant value steps past 1.0.
+    fn next_source_frame(&mut self, source_frame: Self::Frame);
+}
