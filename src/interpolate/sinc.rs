6aa1a12d278bbe6b1a349f3edfb6d976b66914b0
Mark LeMoine <linclelinkpart5@gmail.com>
2021-02-24 23:59:50 -0800

    Flesh out `Interpolator` logic for `Sinc`, need to revisit `Fixed` to ensure wrapped indexing

--- src/interpolate/sinc.rs
+++ src/interpolate/sinc.rs
@@ -1,5 +1,8 @@
+use core::f64::consts::PI;
+
 use crate::{Duplex, Frame, Sample};
 use crate::buffer::{Buffer, Fixed};
+use crate::interpolate::Interpolator;
 
 /// An [`Interpolator`] that uses sinc interpolation on a window of [`Frame`]s.
 ///
@@ -35,6 +38,73 @@ where
         Self {
             buffer: Fixed::from(buffer),
             idx: 0,
+        }
+    }
+
+    #[inline]
+    fn depth(&self) -> usize {
+        self.buffer.capacity() / 2
+    }
+}
+
+impl<F, B, const N: usize> Interpolator<N> for Sinc<F, B, N>
+where
+    B: Buffer<Item = F>,
+    F: Frame<N>,
+    F::Sample: Duplex<f64>,
+{
+    type Frame = F;
+
+    fn interpolate(&self, x: f64) -> Self::Frame {
+        // let phil = x;
+        // let phir = 1.0 - x;
+        // let nl = self.idx;
+        // let nr = self.idx + 1;
+        // let depth = self.depth();
+
+        // let rightmost = nl + depth;
+        // let leftmost = nr as isize - depth as isize;
+        // let max_depth = if rightmost >= self.frames.len() {
+        //     self.frames.len() - depth
+        // } else if leftmost < 0 {
+        //     (depth as isize + leftmost) as usize
+        // } else {
+        //     depth
+        // };
+
+        // (0..max_depth).fold(Self::Frame::EQUILIBRIUM, |mut v, n| {
+        //     v = {
+        //         let a = PI * (phil + n as f64);
+        //         let first = if a == 0.0 { 1.0 } else { sin(a) / a };
+        //         let second = 0.5 + 0.5 * cos(a / depth as f64);
+        //         v.zip_map(self.frames[nl - n], |vs, r_lag| {
+        //             vs.add_amp(
+        //                 (first * second * r_lag.to_sample::<f64>())
+        //                     .to_sample::<<Self::Frame as Frame>::Sample>()
+        //                     .to_signed_sample(),
+        //             )
+        //         })
+        //     };
+
+        //     let a = PI * (phir + n as f64);
+        //     let first = if a == 0.0 { 1.0 } else { sin(a) / a };
+        //     let second = 0.5 + 0.5 * cos(a / depth as f64);
+        //     v.zip_map(self.frames[nr + n], |vs, r_lag| {
+        //         vs.add_amp(
+        //             (first * second * r_lag.to_sample::<f64>())
+        //                 .to_sample::<<Self::Frame as Frame>::Sample>()
+        //                 .to_signed_sample(),
+        //         )
+        //     })
+        // })
+
+        todo!()
+    }
+
+    fn advance(&mut self, next_frame: Self::Frame) {
+        let _prev_frame = self.buffer.push(next_frame);
+        if self.idx < self.depth() {
+            self.idx += 1;
         }
     }
 }
