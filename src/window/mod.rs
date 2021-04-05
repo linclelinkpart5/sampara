bdb971feafc613190e338a04c8d406e0f5e5b7bc
Mark LeMoine <linclelinkpart5@gmail.com>
2021-04-05 15:24:00 -0700

    Create `WindowFunc`, add simple window impls

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,3 +1,32 @@
 use num_traits::Float;
 
 use crate::buffer::Buffer;
+
+pub trait WindowFunc<F: Float> {
+    /// Given a value in the interval [-1.0, 1.0], returns the value of the
+    /// window function at that point.
+    fn calc(x: F) -> F;
+
+    // fn fill_buffer<B>(buffer: &mut B)
+    // where
+    //     B: Buffer<Item = F>,
+    // {
+    //     todo!();
+    // }
+}
+
+pub struct Rectangle;
+
+impl<F: Float> WindowFunc<F> for Rectangle {
+    fn calc(_x: F) -> F {
+        F::one()
+    }
+}
+
+pub struct Triangle;
+
+impl<F: Float> WindowFunc<F> for Triangle {
+    fn calc(x: F) -> F {
+        F::one() - x.abs()
+    }
+}
