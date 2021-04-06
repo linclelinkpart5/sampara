10a043f3c4b1e24e04c11c940af5a067fad704fc
Mark LeMoine <linclelinkpart5@gmail.com>
2021-04-05 23:46:55 -0700

    Make `WindowFunc::calc` an instance method, add `IterCase`, clean up `Iter`

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,13 +1,19 @@
 use std::ops::Range;
+use std::option::IntoIter as OptionIntoIter;
 
 use num_traits::Float;
 
-use crate::buffer::Buffer;
+// use crate::buffer::Buffer;
+
+enum End {
+    Front,
+    Back,
+}
 
 pub trait WindowFunc<F: Float> {
     /// Given a value in the interval [-1.0, 1.0], returns the value of the
     /// window function at that point.
-    fn calc(x: F) -> F;
+    fn calc(&self, x: F) -> F;
 
     // fn fill_buffer<B>(buffer: &mut B)
     // where
@@ -16,22 +22,18 @@ pub trait WindowFunc<F: Float> {
     //     todo!();
     // }
 
-    fn iter(len: usize) -> Iter<Self, F>
+    fn iter(self, len: usize) -> Iter<Self, F>
     where
         Self: Sized,
     {
-        Iter {
-            range: 0..len,
-            len,
-            _marker: Default::default(),
-        }
+        Iter(IterCase::new(len, self))
     }
 }
 
 pub struct Rectangle;
 
 impl<F: Float> WindowFunc<F> for Rectangle {
-    fn calc(_x: F) -> F {
+    fn calc(&self, _x: F) -> F {
         F::one()
     }
 }
@@ -39,8 +41,109 @@ impl<F: Float> WindowFunc<F> for Rectangle {
 pub struct Triangle;
 
 impl<F: Float> WindowFunc<F> for Triangle {
-    fn calc(x: F) -> F {
+    fn calc(&self, x: F) -> F {
         F::one() - x.abs()
+    }
+}
+
+#[inline]
+fn calc_at<W, F>(i: usize, factor: F, wf: &W) -> F
+where
+    W: WindowFunc<F>,
+    F: Float,
+{
+    let x = factor * F::from(i).unwrap() - F::one();
+    wf.calc(x)
+}
+
+enum IterCase<W, F>
+where
+    W: WindowFunc<F>,
+    F: Float,
+{
+    ZeroOne(OptionIntoIter<()>),
+    Normal(Range<usize>, F, W),
+}
+
+impl<W, F> IterCase<W, F>
+where
+    W: WindowFunc<F>,
+    F: Float,
+{
+    fn new(len: usize, windower: W) -> Self {
+        match len {
+            0 => Self::ZeroOne(None.into_iter()),
+            1 => Self::ZeroOne(Some(()).into_iter()),
+            n => {
+                let factor = F::from(2).unwrap() / F::from(n - 1).unwrap();
+                Self::Normal(0..n, factor, windower)
+            },
+        }
+    }
+
+    fn advance(&mut self, end: End) -> Option<<Self as Iterator>::Item> {
+        match self {
+            Self::ZeroOne(it) => {
+                let opt = match end {
+                    End::Front => it.next(),
+                    End::Back => it.next_back(),
+                };
+
+                // TODO: Should this be zero or one?
+                opt.map(|_| F::zero())
+            },
+
+            Self::Normal(range, factor, wf) => {
+                let i = match end {
+                    End::Front => range.next(),
+                    End::Back => range.next_back(),
+                }?;
+
+                Some(calc_at(i, *factor, wf))
+            },
+        }
+    }
+}
+
+impl<W, F> Iterator for IterCase<W, F>
+where
+    W: WindowFunc<F>,
+    F: Float,
+{
+    type Item = F;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.advance(End::Front)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        match self {
+            Self::ZeroOne(it) => it.size_hint(),
+            Self::Normal(range, ..) => range.size_hint(),
+        }
+    }
+}
+
+impl<W, F> ExactSizeIterator for IterCase<W, F>
+where
+    W: WindowFunc<F>,
+    F: Float,
+{
+    fn len(&self) -> usize {
+        match self {
+            Self::ZeroOne(it) => it.len(),
+            Self::Normal(range, ..) => range.len(),
+        }
+    }
+}
+
+impl<W, F> DoubleEndedIterator for IterCase<W, F>
+where
+    W: WindowFunc<F>,
+    F: Float,
+{
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.advance(End::Back)
     }
 }
 
@@ -56,7 +159,7 @@ impl<F: Float> WindowFunc<F> for Triangle {
 ///
 /// fn main() {
 ///     // An odd number of points produces a value at `x = 0.0` exactly.
-///     let mut iter = Triangle::iter(5);
+///     let mut iter = Triangle.iter(5);
 ///
 ///     assert_eq!(iter.next(), Some(0.0f64));
 ///     assert_eq!(iter.next(), Some(0.5));
@@ -66,7 +169,7 @@ impl<F: Float> WindowFunc<F> for Triangle {
 ///     assert_eq!(iter.next(), None);
 ///
 ///     // An even number of points misses the `x = 0.0` point slightly.
-///     let mut iter = Triangle::iter(4);
+///     let mut iter = Triangle.iter(4);
 ///
 ///     assert_eq!(iter.next(), Some(0.0f64));
 ///     assert_eq!(iter.next(), Some(0.6666666666666666));
@@ -75,56 +178,29 @@ impl<F: Float> WindowFunc<F> for Triangle {
 ///     assert_eq!(iter.next(), None);
 ///
 ///     // Two points produce values at `x = -1.0` and `x = 1.0`.
-///     let mut iter = Triangle::iter(2);
+///     let mut iter = Triangle.iter(2);
 ///
 ///     assert_eq!(iter.next(), Some(0.0f64));
 ///     assert_eq!(iter.next(), Some(0.0));
 ///     assert_eq!(iter.next(), None);
 ///
 ///     // One point always yields a value of `0.0`.
-///     let mut iter = Triangle::iter(1);
+///     let mut iter = Triangle.iter(1);
 ///
 ///     assert_eq!(iter.next(), Some(0.0f64));
 ///     assert_eq!(iter.next(), None);
 ///
 ///     // Zero points is an empty iterator.
-///     let mut iter: Iter<_, f64> = Triangle::iter(0);
+///     let mut iter: Iter<_, f64> = Triangle.iter(0);
 ///
 ///     assert_eq!(iter.next(), None);
 /// }
 /// ```
-pub struct Iter<W, F>
+pub struct Iter<W, F>(IterCase<W, F>)
 where
     W: WindowFunc<F>,
     F: Float,
-{
-    range: Range<usize>,
-    len: usize,
-    _marker: std::marker::PhantomData<(W, F)>,
-}
-
-impl<W, F> Iter<W, F>
-where
-    W: WindowFunc<F>,
-    F: Float,
-{
-    fn idx(&self, i: usize) -> F {
-        match self.len {
-            0 => unreachable!(),
-
-            // TODO: Should this be zero or one?
-            1 => F::zero(),
-
-            n => {
-                let f = F::from(2).unwrap() / F::from(n - 1).unwrap();
-
-                let x = f * F::from(i).unwrap() - F::one();
-
-                W::calc(x)
-            },
-        }
-    }
-}
+;
 
 impl<W, F> Iterator for Iter<W, F>
 where
@@ -134,12 +210,11 @@ where
     type Item = F;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let i = self.range.next()?;
-        Some(self.idx(i))
+        self.0.next()
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        self.range.size_hint()
+        self.0.size_hint()
     }
 }
 
@@ -149,7 +224,7 @@ where
     F: Float,
 {
     fn len(&self) -> usize {
-        self.range.len()
+        self.0.len()
     }
 }
 
@@ -159,7 +234,6 @@ where
     F: Float,
 {
     fn next_back(&mut self) -> Option<Self::Item> {
-        let i = self.range.next_back()?;
-        Some(self.idx(i))
+        self.0.next_back()
     }
 }
