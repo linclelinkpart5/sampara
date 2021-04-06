9fcbb8e88b595b7e952385e732ad090a74d0515e
Mark LeMoine <linclelinkpart5@gmail.com>
2021-04-05 21:02:43 -0700

    Add `iter` method to `WindowFunc` trait, add doctest for `Iter`, add `ExactSizeIterator` impl to `Iter`

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -13,6 +13,17 @@ pub trait WindowFunc<F: Float> {
     // {
     //     todo!();
     // }
+
+    fn iter(len: usize) -> Iter<Self, F>
+    where
+        Self: Sized,
+    {
+        Iter {
+            i: 0,
+            len,
+            _marker: Default::default(),
+        }
+    }
 }
 
 pub struct Rectangle;
@@ -31,7 +42,55 @@ impl<F: Float> WindowFunc<F> for Triangle {
     }
 }
 
-
+/// An [`Iterator`] that yields the values of a window (via a [`WindowFunc`])
+/// for a given number of points, evenly spaced to exactly span the interval
+/// [-1.0, 1.0].
+///
+/// Iterating over 0 points yields no values. Iterating over 1 point yields 0.0
+/// once, regardless of the chosen [`WindowFunc`].
+///
+/// ```
+/// use sampara::window::{WindowFunc, Triangle, Iter};
+///
+/// fn main() {
+///     // An odd number of points produces a value at `x = 0.0` exactly.
+///     let mut iter = Triangle::iter(5);
+///
+///     assert_eq!(iter.next(), Some(0.0f64));
+///     assert_eq!(iter.next(), Some(0.5));
+///     assert_eq!(iter.next(), Some(1.0)); // x = 0.0
+///     assert_eq!(iter.next(), Some(0.5));
+///     assert_eq!(iter.next(), Some(0.0));
+///     assert_eq!(iter.next(), None);
+///
+///     // An even number of points misses the `x = 0.0` point slightly.
+///     let mut iter = Triangle::iter(4);
+///
+///     assert_eq!(iter.next(), Some(0.0f64));
+///     assert_eq!(iter.next(), Some(0.6666666666666666));
+///     assert_eq!(iter.next(), Some(0.6666666666666667));
+///     assert_eq!(iter.next(), Some(0.0));
+///     assert_eq!(iter.next(), None);
+///
+///     // Two points produce values at `x = -1.0` and `x = 1.0`.
+///     let mut iter = Triangle::iter(2);
+///
+///     assert_eq!(iter.next(), Some(0.0f64));
+///     assert_eq!(iter.next(), Some(0.0));
+///     assert_eq!(iter.next(), None);
+///
+///     // One point always yields a value of `0.0`.
+///     let mut iter = Triangle::iter(1);
+///
+///     assert_eq!(iter.next(), Some(0.0f64));
+///     assert_eq!(iter.next(), None);
+///
+///     // Zero points is an empty iterator.
+///     let mut iter: Iter<_, f64> = Triangle::iter(0);
+///
+///     assert_eq!(iter.next(), None);
+/// }
+/// ```
 pub struct Iter<W, F>
 where
     W: WindowFunc<F>,
@@ -73,4 +132,16 @@ where
             None
         }
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        // This should never underflow.
+        let r = self.len - self.i;
+        (r, Some(r))
+    }
 }
+
+impl<W, F> ExactSizeIterator for Iter<W, F>
+where
+    W: WindowFunc<F>,
+    F: Float,
+{}
