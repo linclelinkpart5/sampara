6a761d2e67413876dc41e9fd803eb2cdc7b41218
Mark LeMoine <linclelinkpart5@gmail.com>
2021-05-30 01:36:03 -0700

    Add `Selector` combinator

--- src/components/combinators.rs
+++ src/components/combinators.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use crate::Frame;
 
 pub trait Combinator<const NL: usize, const NR: usize, const NO: usize> {
@@ -34,8 +36,8 @@ where
     FO: Frame<NO>,
     M: FnMut(FL, FR) -> FO,
 {
-    pub(super) func: M,
-    pub(super) _marker: std::marker::PhantomData<(FL, FR, FO)>,
+    func: M,
+    _marker: PhantomData<(FL, FR, FO)>,
 }
 
 impl<FL, FR, FO, M, const NL: usize, const NR: usize, const NO: usize>
@@ -81,5 +83,84 @@ where
 {
     fn from(func: M) -> Self {
         Self::new(func)
+    }
+}
+
+/// A [`Combinator`] that returns one of the two input [`Frame`]s as determined
+/// by a selector state. This selector state can be changed at runtime in order
+/// to switch between returning the left/right input [`Frame`].
+///
+/// ```
+/// use sampara::Combinator;
+/// use sampara::components::combinators::Selector;
+///
+/// fn main() {
+///     const L: [u8; 3] = [1, 2, 3];
+///     const R: [u8; 3] = [4, 5, 6];
+///
+///     let mut c = Selector::left();
+///     assert_eq!(c.combine(L, R), L);
+///
+///     c.set_right();
+///     assert_eq!(c.combine(L, R), R);
+///
+///     c.toggle();
+///     assert_eq!(c.combine(L, R), L);
+/// }
+/// ```
+pub struct Selector<F, const N: usize>
+where
+    F: Frame<N>,
+{
+    select_right: bool,
+    _marker: PhantomData<F>,
+}
+
+impl<F, const N: usize> Selector<F, N>
+where
+    F: Frame<N>,
+{
+    fn new(r: bool) -> Self {
+        Self {
+            select_right: r,
+            _marker: Default::default(),
+        }
+    }
+
+    pub fn left() -> Self {
+        Self::new(false)
+    }
+
+    pub fn right() -> Self {
+        Self::new(true)
+    }
+
+    pub fn set_left(&mut self) {
+        self.select_right = false
+    }
+
+    pub fn set_right(&mut self) {
+        self.select_right = true
+    }
+
+    pub fn toggle(&mut self) {
+        self.select_right = !self.select_right
+    }
+}
+
+impl<F, const N: usize> Combinator<N, N, N> for Selector<F, N>
+where
+    F: Frame<N>,
+{
+    type InputL = F;
+    type InputR = F;
+    type Output = F;
+
+    fn combine(&mut self, input_l: Self::InputL, input_r: Self::InputR) -> Self::Output {
+        if self.select_right {
+            input_r
+        } else {
+            input_l
+        }
     }
 }
