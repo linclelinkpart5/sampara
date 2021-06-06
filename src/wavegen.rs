926acec8cc8ecb65e21180a131fbd0011c464653
Mark LeMoine <linclelinkpart5@gmail.com>
2021-06-05 22:20:15 -0700

    Reintroduce associated types to `Step`, make `Step` impls generic over `S: FloatSample`, add `Phase` definition

--- src/wavegen.rs
+++ src/wavegen.rs
@@ -1,12 +1,25 @@
+use num_traits::Float;
+
 use crate::{Frame, Signal};
 use crate::sample::FloatSample;
 
-pub trait Step<F, const N: usize>
+// LEARN: Good example of the difference between type generics and associated
+//        types.
+// pub trait OldStep<F, const N: usize>
+// where
+//     F: Frame<N>,
+//     F::Sample: FloatSample,
+// {
+//     fn step(&mut self) -> Option<F>;
+// }
+
+pub trait Step<S, const N: usize>
 where
-    F: Frame<N>,
-    F::Sample: FloatSample,
+    S: FloatSample,
 {
-    fn step(&mut self) -> Option<F>;
+    type Step: Frame<N, Sample = S>;
+
+    fn step(&mut self) -> Option<Self::Step>;
 }
 
 pub struct Fixed<F, const N: usize>(F)
@@ -14,12 +27,14 @@ where
     F: Frame<N>,
     F::Sample: FloatSample;
 
-impl<F, const N: usize> Step<F, N> for Fixed<F, N>
+impl<F, const N: usize> Step<F::Sample, N> for Fixed<F, N>
 where
     F: Frame<N>,
     F::Sample: FloatSample,
 {
-    fn step(&mut self) -> Option<F> {
+    type Step = F;
+
+    fn step(&mut self) -> Option<Self::Step> {
         Some(self.0)
     }
 }
@@ -33,15 +48,17 @@ where
     Deltas(S),
 }
 
-impl<S, const N: usize> Step<S::Frame, N> for VarInner<S, N>
+impl<S, const N: usize> Step<<S::Frame as Frame<N>>::Sample, N> for VarInner<S, N>
 where
     S: Signal<N>,
-    S::Frame: Frame<N, Sample = f64>,
+    <S::Frame as Frame<N>>::Sample: FloatSample,
 {
-    fn step(&mut self) -> Option<S::Frame> {
+    type Step = S::Frame;
+
+    fn step(&mut self) -> Option<Self::Step> {
         match self {
             Self::Hzs(hz_signal, rate) => {
-                hz_signal.next().map(|f| f.mul_amp(1.0 / *rate))
+                hz_signal.next().map(|f| f.mul_amp(rate.recip()))
             },
             Self::Deltas(delta_signal) => {
                 delta_signal.next()
@@ -55,12 +72,23 @@ where
     S: Signal<N>,
     <S::Frame as Frame<N>>::Sample: FloatSample;
 
-impl<S, const N: usize> Step<S::Frame, N> for Variable<S, N>
+impl<S, const N: usize> Step<<S::Frame as Frame<N>>::Sample, N> for Variable<S, N>
 where
     S: Signal<N>,
-    S::Frame: Frame<N, Sample = f64>,
+    <S::Frame as Frame<N>>::Sample: FloatSample,
 {
-    fn step(&mut self) -> Option<S::Frame> {
+    type Step = S::Frame;
+
+    fn step(&mut self) -> Option<Self::Step> {
         self.0.step()
     }
+}
+
+pub struct Phase<S, X, const N: usize>
+where
+    S: FloatSample,
+    X: Step<S, N>,
+{
+    stepper: X,
+    accum: X::Step,
 }
