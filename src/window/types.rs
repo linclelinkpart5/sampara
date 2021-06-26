0ad9eaf9fcd9d402a91e4f1f2c214471ca1a9405
Mark LeMoine <linclelinkpart5@gmail.com>
2021-06-26 00:48:46 -0700

    Replace doc tests in `Window::types` with proptests

--- src/window/types.rs
+++ src/window/types.rs
@@ -4,17 +4,6 @@ use crate::sample::FloatSample;
 use crate::window::Window;
 
 /// Represents a rectangular (aka boxcar) window.
-///
-/// ```
-/// use sampara::window::Window;
-/// use sampara::window::types::Rectangle;
-///
-/// fn main() {
-///     let mut buffer = [-1.0; 16];
-///     Window::fill(Rectangle, &mut buffer);
-///     assert_eq!(buffer, [1.0; 16]);
-/// }
-/// ```
 pub struct Rectangle;
 
 impl<F: FloatSample> Window<F> for Rectangle {
@@ -24,34 +13,6 @@ impl<F: FloatSample> Window<F> for Rectangle {
 }
 
 /// Represents a triangular window.
-///
-/// ```
-/// use sampara::window::Window;
-/// use sampara::window::types::Triangle;
-///
-/// fn main() {
-///     let mut buffer = [-1.0; 16];
-///     Window::fill(Triangle, &mut buffer);
-///     assert_eq!(buffer, [
-///         0.0000000000000000,
-///         0.1333333333333333,
-///         0.2666666666666666,
-///         0.4000000000000000,
-///         0.5333333333333333,
-///         0.6666666666666666,
-///         0.8000000000000000,
-///         0.9333333333333333,
-///         0.9333333333333333,
-///         0.8000000000000000,
-///         0.6666666666666667,
-///         0.5333333333333334,
-///         0.3999999999999999,
-///         0.2666666666666666,
-///         0.1333333333333333,
-///         0.0000000000000000,
-///     ]);
-/// }
-/// ```
 pub struct Triangle;
 
 impl<F: FloatSample> Window<F> for Triangle {
@@ -61,34 +22,6 @@ impl<F: FloatSample> Window<F> for Triangle {
 }
 
 /// Represents a Welch window.
-///
-/// ```
-/// use sampara::window::Window;
-/// use sampara::window::types::Welch;
-///
-/// fn main() {
-///     let mut buffer = [-1.0; 16];
-///     Window::fill(Welch, &mut buffer);
-///     assert_eq!(buffer, [
-///         0.00000000000000000,
-///         0.24888888888888883,
-///         0.46222222222222210,
-///         0.64000000000000000,
-///         0.78222222222222220,
-///         0.88888888888888880,
-///         0.96000000000000000,
-///         0.99555555555555550,
-///         0.99555555555555550,
-///         0.96000000000000000,
-///         0.88888888888888900,
-///         0.78222222222222240,
-///         0.63999999999999990,
-///         0.46222222222222210,
-///         0.24888888888888883,
-///         0.00000000000000000,
-///     ]);
-/// }
-/// ```
 pub struct Welch;
 
 impl<F: FloatSample> Window<F> for Welch {
@@ -99,34 +32,6 @@ impl<F: FloatSample> Window<F> for Welch {
 }
 
 /// Represents a Hann window.
-///
-/// ```
-/// use sampara::window::Window;
-/// use sampara::window::types::Hann;
-///
-/// fn main() {
-///     let mut buffer = [-1.0; 16];
-///     Window::fill(Hann, &mut buffer);
-///     assert_eq!(buffer, [
-///         0.00000000000000000,
-///         0.04322727117869957,
-///         0.16543469682057088,
-///         0.34549150281252630,
-///         0.55226423163382670,
-///         0.74999999999999990,
-///         0.90450849718747370,
-///         0.98907380036690280,
-///         0.98907380036690280,
-///         0.90450849718747370,
-///         0.75000000000000020,
-///         0.55226423163382710,
-///         0.34549150281252640,
-///         0.16543469682057077,
-///         0.04322727117869951,
-///         0.00000000000000000,
-///     ]);
-/// }
-/// ```
 pub struct Hann;
 
 impl<F: FloatSample> Window<F> for Hann {
@@ -136,34 +41,6 @@ impl<F: FloatSample> Window<F> for Hann {
 }
 
 /// Represents a Blackman window.
-///
-/// ```
-/// use sampara::window::Window;
-/// use sampara::window::types::Blackman;
-///
-/// fn main() {
-///     let mut buffer = [-1.0; 16];
-///     Window::fill(Blackman, &mut buffer);
-///     assert_eq!(buffer, [
-///         -0.000000000000000013877787807814457,
-///         0.016757719687408210,
-///         0.077072419759158600,
-///         0.200770143262530450,
-///         0.394012423575122200,
-///         0.629999999999999900,
-///         0.849229856737469400,
-///         0.982157436978310800,
-///         0.982157436978310800,
-///         0.849229856737469500,
-///         0.630000000000000200,
-///         0.394012423575122670,
-///         0.200770143262530560,
-///         0.077072419759158530,
-///         0.016757719687408162,
-///         -0.000000000000000013877787807814457,
-///     ]);
-/// }
-/// ```
 pub struct Blackman;
 
 impl<F: FloatSample> Window<F> for Blackman {
@@ -181,5 +58,83 @@ impl<F: FloatSample> Window<F> for Blackman {
         let c2 = ((F::TAU() + F::TAU()) * x).cos();
 
         a0 - a1 * c1 + a2 * c2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::f64::consts::TAU;
+
+    use proptest::prelude::*;
+
+    fn arb_delta() -> impl Strategy<Value = f64> {
+        (1u64..)
+        .prop_flat_map(|len| {
+            (Just(len), 0..=len)
+        })
+        .prop_map(|(len, idx)| idx as f64 / len as f64)
+    }
+
+    proptest! {
+        #[test]
+        fn prop_rectangle(x in arb_delta()) {
+            let wf = Rectangle;
+
+            let expected = 1.0;
+            let produced = wf.calc(x);
+
+            assert_eq!(expected, produced);
+        }
+
+        #[test]
+        fn prop_triangle(x in arb_delta()) {
+            let wf = Triangle;
+
+            let expected = 1.0 - (2.0 * x - 1.0).abs();
+            let produced = wf.calc(x);
+
+            assert_eq!(expected, produced);
+        }
+
+        #[test]
+        fn prop_welch(x in arb_delta()) {
+            let wf = Welch;
+
+            let i = 2.0 * x - 1.0;
+            let expected = 1.0 - (i * i);
+            let produced = wf.calc(x);
+
+            assert_eq!(expected, produced);
+        }
+
+        #[test]
+        fn prop_hann(x in arb_delta()) {
+            let wf = Hann;
+
+            let expected = (1.0 - (TAU * x).cos()) * 0.5;
+            let produced = wf.calc(x);
+
+            assert_eq!(expected, produced);
+        }
+
+        #[test]
+        fn prop_blackman(x in arb_delta()) {
+            let wf = Blackman;
+
+            const A: f64 = 0.16;
+            const A0: f64 = 0.5 * (1.0 - A);
+            const A1: f64 = 0.5;
+            const A2: f64 = 0.5 * A;
+
+            let c1 = (TAU * x).cos();
+            let c2 = (2.0 * TAU * x).cos();
+
+            let expected = A0 - A1 * c1 + A2 * c2;
+            let produced = wf.calc(x);
+
+            assert_eq!(expected, produced);
+        }
     }
 }
