8fac70798a103b61d436b0b47fddf88768d235a6
Mark LeMoine <linclelinkpart5@gmail.com>
2021-06-27 12:40:51 -0700

    Create `SlidingStat` trait for future ergonomics

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -11,6 +11,175 @@ const NO_SQRT: bool = false;
 const DO_POW2: bool = true;
 const NO_POW2: bool = false;
 
+/// Types that perform a calculation using a sliding window (ring buffer) of
+/// input data.
+pub trait SlidingStat<F, B, const N: usize>: From<B> + Processor<N, N, Input = F, Output = F>
+where
+    F: Frame<N>,
+    B: Buffer<Item = F>,
+{
+    fn empty(buffer: B) -> Self;
+    fn len(&self) -> usize;
+    fn reset(&mut self);
+    fn fill(&mut self, fill_val: F);
+    fn fill_with<M: FnMut() -> F>(&mut self, fill_func: M);
+    fn advance(&mut self, input: F);
+    fn current(&self) -> F;
+}
+
+impl<F, B, const N: usize, const SQRT: bool, const POW2: bool> SlidingStat<F, B, N> for StatsInner<F, B, N, SQRT, POW2>
+where
+    F: Frame<N>,
+    F::Sample: FloatSample,
+    B: Buffer<Item = F>,
+{
+    #[inline]
+    fn empty(buffer: B) -> Self {
+        let mut new = Self {
+            window: Fixed::from(buffer),
+            sum: Frame::EQUILIBRIUM,
+        };
+
+        new.__reset();
+
+        new
+    }
+
+    #[inline]
+    fn len(&self) -> usize {
+        self.window.capacity()
+    }
+
+    #[inline]
+    fn reset(&mut self) {
+        // ASSUME: All float samples have an equilibrium of 0. That way this
+        // code as written works for any combo of (SQRT, POW2).
+        self.window.fill(Frame::EQUILIBRIUM);
+        self.sum = Frame::EQUILIBRIUM;
+    }
+
+    #[inline]
+    fn fill(&mut self, fill_val: F) {
+        let mut fill_val = fill_val;
+
+        if POW2 {
+            // Calculate the squared frame, as that is what will actually be
+            // stored in the window.
+            fill_val.transform(|x| x * x);
+        }
+
+        self.window.fill(fill_val);
+
+        // Since the buffer is filled with a constant value, just multiply to
+        // calculate the sum.
+        let len_f: F::Sample = Sample::from_sample(self.__len() as f32);
+        self.sum = fill_val.mul_amp(len_f);
+    }
+
+    #[inline]
+    fn fill_with<M>(&mut self, fill_func: M)
+    where
+        M: FnMut() -> F,
+    {
+        let mut fill_func = fill_func;
+        let mut sum = F::EQUILIBRIUM;
+
+        let prepped_fill_func = || {
+            let mut f = fill_func();
+
+            if POW2 {
+                // Square the frame.
+                f.transform(|x| x * x);
+            }
+
+            // Before yielding the squared frame, add it to the running sum.
+            sum.add_assign_frame(f.into_signed_frame());
+
+            f
+        };
+
+        self.window.fill_with(prepped_fill_func);
+        self.sum = sum;
+    }
+
+    #[inline]
+    fn advance(&mut self, input: F) {
+        let mut input = input;
+
+        if POW2 {
+            // Calculate the square of the new frame and push onto the buffer.
+            input.transform(|x| x * x);
+        }
+
+        let popped = self.window.push(input);
+
+        // Add the new input and subtract the popped frame from the sum.
+        self.sum
+            .add_assign_frame(input.into_signed_frame())
+            .sub_assign_frame(popped.into_signed_frame());
+
+        if SQRT {
+            // In case of floating point rounding errors, floor at equilibrium.
+            self.sum.transform(|x| x.max(Sample::EQUILIBRIUM));
+        }
+    }
+
+    #[inline]
+    fn current(&self) -> F {
+        let len_f = Sample::from_sample(self.__len() as f32);
+        let mut ret: F = self.sum.apply(|s| s / len_f);
+
+        if SQRT {
+            ret.transform(Float::sqrt);
+        }
+
+        ret
+    }
+}
+
+impl<F, B, const N: usize, const SQRT: bool, const POW2: bool> From<B> for StatsInner<F, B, N, SQRT, POW2>
+where
+    F: Frame<N>,
+    F::Sample: FloatSample,
+    B: Buffer<Item = F>,
+{
+    fn from(buffer: B) -> Self {
+        let mut buffer = buffer;
+        let mut sum = F::EQUILIBRIUM;
+
+        for frame in buffer.as_mut().iter_mut() {
+            if POW2 {
+                // Since the passed-in buffer has raw frames, square them
+                // in-place.
+                frame.transform(|x| x * x);
+            }
+
+            sum.add_assign_frame(frame.into_signed_frame());
+        }
+
+        Self {
+            window: Fixed::from(buffer),
+            sum,
+        }
+    }
+}
+
+impl<F, B, const N: usize, const SQRT: bool, const POW2: bool> Processor<N, N> for StatsInner<F, B, N, SQRT, POW2>
+where
+    F: Frame<N>,
+    F::Sample: FloatSample,
+    B: Buffer<Item = F>,
+{
+    type Input = F;
+    type Output = F;
+
+    #[inline]
+    fn process(&mut self, input: F) -> F {
+        self.advance(input);
+        self.current()
+    }
+}
+
 #[derive(Clone)]
 struct StatsInner<F, B, const N: usize, const SQRT: bool, const POW2: bool>
 where
