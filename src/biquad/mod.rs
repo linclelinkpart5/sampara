70f65ed58410bfca66c6089fc9cc192053eb1e44
Mark LeMoine <linclelinkpart5@gmail.com>
2024-12-06 14:14:43 -0800

    Add `biquad::Coefficients`

--- src/biquad/mod.rs
+++ src/biquad/mod.rs
@@ -1,1 +1,20 @@
+use crate::sample::FloatSample;
 
+/// Coefficients for a digital biquad filter.
+///
+/// It is assumed that the `a0` coefficient is always normalized to 1.0,
+/// and thus not included.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub struct Coefficients<X>
+where
+    X: FloatSample,
+{
+    // Transfer function numerator coefficients.
+    pub b0: X,
+    pub b1: X,
+    pub b2: X,
+
+    // Transfer function denominator coefficients.
+    pub a1: X,
+    pub a2: X,
+}
